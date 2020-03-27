f8cea5c0c3cc80429af734c398e1c76c2fefca31
dmitry.navitski <arbalestindigo@gmail.com>
2020-03-27 15:29:51 +0300

    Initial Commit

---
+++ PapaKnows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PapaKnows
+//
+//  Created by Dmitry on 27.03.2020.
+//  Copyright © 2020 Dmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
